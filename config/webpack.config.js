efa2127ce17411303e8228d319d9872680ab7cb3
JeanFab0307 <jfrandriatsimivony@gmail.com>
2024-10-17 09:27:14 +0300

    feat: implement basic config for first run
    added webpack config and jest config as well as basic file like index.js
    and App.js for a first run.

---
+++ config/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  target: 'web',
+  entry: {
+    main: path.resolve(__dirname, '../src/index.js'),
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(gif|svg|png|jpg|jpeg)$/i,
+				use: [
+          'file-loader',
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							bypassOnDebug: true,
+							disable: true,
+						},
+					},
+				],
+      },
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.resolve("./dist"),
+    compress: true,
+    port: 8564,
+  },
+  performance: {
+    maxAssetSize: 1000000,
+  },
+  devtool: 'inline-source-map',
+};
---
+++ src/App/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    }
+
+  render() {
+
+    return (
+      <div>
+        App
+      </div>
+    );
+  }
+}
+
+export default App;
