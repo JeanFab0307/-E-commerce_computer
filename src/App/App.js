a030ba61adbf3e5a1f24a60f0333a6a68c4b9f2b
JeanFab0307 <jfrandriatsimivony@gmail.com>
2024-10-21 10:16:24 +0300

    feat: create basic layout
    added a simple header and footer to the homepage

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import Proptypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    }
+  }
 
   render() {
-
     return (
-      <div>
-        App
+      <div className={css(styles.app)}>
+        <Header />
+        <div style={{height: '50vh'}}>
+          Body
+        </div>
+        <Footer />
       </div>
-    );
-  }
+  );
 }
+}
+
+const styles = StyleSheet.create({
+  app: {
+    fontFamily: 'Open Sans, Helvetica Neue, Helvetica, Arial, sans-serif'
+  }
+});
 
 export default App;
